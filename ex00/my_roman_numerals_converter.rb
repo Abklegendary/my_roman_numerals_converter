f26c69c19f31bb1dccc69de29f2e46f6b06285c5
abubakar hamza <hamza_a@qwasar.io>
2023-01-29 20:09:48 +0000

    my roman numerals converter in ruby

--- ex00/my_roman_numerals_converter.rb
+++ ex00/my_roman_numerals_converter.rb
@@ -15,6 +15,12 @@ roman_mapping = {
     1 => "I"
 
 }
+#variable "roman" as an empty string, then iterates over the elements in the hash "roman_mapping".
+ #For each key-value pair (k, v) in the hash, it performs the following actions:
+
+#Determines the number of times k goes into num using the expression (num / k).
+#For that number of times, it appends v to the string "roman".
+#Finally, it subtracts k times the number of times it went into num from num.
 roman = ""
 roman_mapping.each do |k, v|
     (numbers/k).times {roman << v; numbers -= k}
